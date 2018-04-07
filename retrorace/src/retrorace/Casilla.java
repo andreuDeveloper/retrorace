9a6c00a0a89ae9421d4d0c771a5e8cce830152a3
Andrés <arsg9@DESKTOP-9GRBCJG>
2018-04-07 20:26:16 +0200

    V1 Casilla

--- retrorace/src/retrorace/Casilla.java
+++ retrorace/src/retrorace/Casilla.java
@@ -5,10 +5,57 @@
  */
 package retrorace;
 
+import javax.swing.Icon;
+
 /**
  *
  * @author Andrés
  */
 public class Casilla {
+    private int id;
+    private Icon img,imgAux;
+    private String propiedad;
+
+    public Casilla(int id, Icon img, Icon imgAux, String propiedad) {
+        this.id = id;
+        this.img = img;
+        this.imgAux = imgAux;
+        this.propiedad = propiedad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Icon getImg() {
+        return img;
+    }
+
+    public void setImg(Icon img) {
+        this.img = img;
+    }
+
+    public Icon getImgAux() {
+        return imgAux;
+    }
+
+    public void setImgAux(Icon imgAux) {
+        this.imgAux = imgAux;
+    }
+
+    public String getPropiedad() {
+        return propiedad;
+    }
+
+    public void setPropiedad(String propiedad) {
+        this.propiedad = propiedad;
+    }
+    
+    
+    
     
 }
