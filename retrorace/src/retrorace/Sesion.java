3d816cfda9ff542d2b869a744fcef19bdf3d75e4
Ramon <ramon_dc_94@hotmail.com>
2018-04-29 12:03:23 +0200

    Añadido panel Menu, panel Eleccion Mapa y panel Gamescreen

--- retrorace/src/retrorace/Sesion.java
+++ retrorace/src/retrorace/Sesion.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
  */
 public class Sesion {
     
-    private ArrayList mapas;
+    private ArrayList<Mapa> mapas;
     private Partida partida;
     private GUI gui;
     private String username;
@@ -23,7 +23,27 @@ public class Sesion {
     public Sesion(GUI gui,String user){
         this.gui = gui;
         this.username=user;
+        this.mapas=new ArrayList();
+        this.gui.setSesion(this);
     }
+    
+    /*GETTERS Y SETERS*/
+    public String getUsername() {
+        return username;
+    }
+
+    public ArrayList<Mapa> getMapas() {
+        return mapas;
+    }
+
+    
+    private void loadMaps(){
+        /*ArrayList<Mapa> mapas;
+        Gson gson = new Gson();
+        mapas = gson.fromJson(new FileReader("data/mapas.json"), new TypeToken<List<Mapa>>() {
+        }.getType());*/
+    }
+    
     
     
     
